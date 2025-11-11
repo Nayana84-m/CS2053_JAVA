465cf81420154472239003fcf7865f3914b0c4b2
Nayana.M <nayanamr0408@gmail.com>
2025-11-11 23:16:44 +0530

    Deleted Unwanted .java file

--- CommandLine.java
+++
@@ -1,7 +0,0 @@
-class CommandLine {
- public static void main(String args[]) {
- System.out.println("the number of arguments are "+ args.length);
- for(int i=0; i<args.length; i++)
- System.out.println("args[" + i + "]: " + args[i]);
- }
-}
